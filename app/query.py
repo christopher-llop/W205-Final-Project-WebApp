abb4a564bb735cf158b0bd3e9f615d61ac02a5a0
Christopher Llop <christopher.llop@gmail.com>
2015-04-18 17:05:32 -0400

    Heroku take 13

--- app/query.py
+++ app/query.py
@@ -14,36 +14,8 @@ def patch_ingred(ingredient):
     return ingredient
 
 def extract_key_ingred(ingredient):
-    measures = ['ounce','ounces','cup','cups','pound','pounds','kilos',
-            'grams','gram','kilo','bag','bags','teaspoon',
-            'teaspoons','tablespoon','tablespoons','tbsp','tbsps',
-            'lbs','kg','kilogram','kilograms','can','cans',
-            'tsp','tsps','oz','pint','pt','pints','pack','packs','packed',
-            'pinch']
 
-    english_sw = nltk.corpus.stopwords.words('english')
-
-    methods = ['grounded','crushed','chopped', 'cored', 'peeled', 'sliced',
-               'squeezed','diced','divided','softened','thawed','needed',
-               'drained','rinsed','beaten','husked','cleaned','mashed','melted',
-               'dried','processed','grated','frying','chopp','chuncked','chunk']
-
-    ingredient = ingredient.strip().lower()
-    ingredient = patch_ingred(ingredient)
-    #print "original ingredients {}".format(ingredient)
-    tokenizer = RegexpTokenizer(r'\w+')
-    tkn = tokenizer.tokenize(ingredient)
-    ps = PorterStemmer()
-    tkn = [ps.stem(t) for t in tkn if t not in measures and t not in methods and t not in english_sw]
-    tagged = nltk.pos_tag(tkn)
-    key_words = [w for w,tag in tagged if tag != 'LS' and tag != 'CD']
-    np = [w for w,tag in tagged if tag == 'NN'] # noun phrase
-    np = ' '.join(np)
-    if np not in key_words and len(np) > 0 and len(np.split()) <= 3:
-        key_words.append(np)
-    return key_words
-
-    return 0
+    return "returning"
 
 ##eps2= coll.find({'ingredient': 'lemon' })
 ##Ingred2 = []for q in eps2:
