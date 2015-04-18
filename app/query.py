0c5cd2da049f2d51fcb1755817c4b63992cd8609
Christopher Llop <christopher.llop@gmail.com>
2015-04-18 16:39:06 -0400

    Heroku take 6

--- app/query.py
+++ app/query.py
@@ -3,6 +3,21 @@ import nltk
 from nltk import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 
+measures = ['ounce','ounces','cup','cups','pound','pounds','kilos',\
+            'grams','gram','kilo','bag','bags','teaspoon',\
+            'teaspoons','tablespoon','tablespoons','tbsp','tbsps',\
+            'lbs','kg','kilogram','kilograms','can','cans',\
+            'tsp','tsps','oz','pint','pt','pints','pack','packs','packed',\
+            'pinch']
+
+english_sw = nltk.corpus.stopwords.words('english')
+
+methods = ['grounded','crushed','chopped', 'cored', 'peeled', 'sliced',\
+           'squeezed','diced','divided','softened','thawed','needed',\
+           'drained','rinsed','beaten','husked','cleaned','mashed','melted',\
+           'dried','processed','grated','frying','chopp','chuncked','chunk']
+
+
 
 def dead_query(query_string):
     results = []
