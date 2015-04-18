f4cfcccd808daa0fdb7dab8b89b8d8f070be1697
Christopher Llop <christopher.llop@gmail.com>
2015-04-18 14:29:03 -0400

    split into query

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm, PostForm
 from config import POSTS_PER_PAGE, MAX_PAGES_DISPLAYED
+from .query import dead_query
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -9,84 +10,10 @@ from config import POSTS_PER_PAGE, MAX_PAGES_DISPLAYED
 def index(CurrentPage = 1):
     form = PostForm()
     if form.validate_on_submit():
-        flash('Search requested for Keywords="%s"' %
-              (form.post.data))
+        flash('Search requested for Keywords="%s"' % (form.post.data))
         return redirect('/index')
     user = {'nickname': 'Friend'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'site': {'title': 'Omelette Du Fromage',
-                     'address': 'http://www.urbandictionary.com/define.php?term=Omelette+du+fromage',
-                     'address_short':'urbandictionary.com' },
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'site': {'title': 'Chicken Parmesan',
-                     'address': 'http://www.simplyrecipes.com/recipes/chicken_parmesan/',
-                     'address_short':'simplyrecipes.com' },
-            'body': 'Here is some more sample query text!'
-        },
-        {
-            'site': {'title': 'Franks Wing Dip',
-                     'address': 'https://www.franksredhot.com/recipes/franks-redhot-buffalo-chicken-dip-RE1242-1',
-                     'address_short':'urbandictionary.com' },
-            'body': 'Uh oh! We are not currently capturing any data to put here'
-        },
-        {
-            'site': {'title': 'Omelette Du Fromage',
-                     'address': 'http://www.urbandictionary.com/define.php?term=Omelette+du+fromage',
-                     'address_short':'urbandictionary.com' },
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'site': {'title': 'Chicken Parmesan',
-                     'address': 'http://www.simplyrecipes.com/recipes/chicken_parmesan/',
-                     'address_short':'simplyrecipes.com' },
-            'body': 'Here is some more sample query text!'
-        },
-        {
-            'site': {'title': 'Franks Wing Dip',
-                     'address': 'https://www.franksredhot.com/recipes/franks-redhot-buffalo-chicken-dip-RE1242-1',
-                     'address_short':'urbandictionary.com' },
-            'body': 'Uh oh! We are not currently capturing any data to put here'
-        },
-        {
-            'site': {'title': 'Omelette Du Fromage',
-                     'address': 'http://www.urbandictionary.com/define.php?term=Omelette+du+fromage',
-                     'address_short':'urbandictionary.com' },
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'site': {'title': 'Chicken Parmesan',
-                     'address': 'http://www.simplyrecipes.com/recipes/chicken_parmesan/',
-                     'address_short':'simplyrecipes.com' },
-            'body': 'Here is some more sample query text!'
-        },
-        {
-            'site': {'title': 'Franks Wing Dip',
-                     'address': 'https://www.franksredhot.com/recipes/franks-redhot-buffalo-chicken-dip-RE1242-1',
-                     'address_short':'urbandictionary.com' },
-            'body': 'Uh oh! We are not currently capturing any data to put here'
-        },
-        {
-            'site': {'title': 'Omelette Du Fromage',
-                     'address': 'http://www.urbandictionary.com/define.php?term=Omelette+du+fromage',
-                     'address_short':'urbandictionary.com' },
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'site': {'title': 'Chicken Parmesan',
-                     'address': 'http://www.simplyrecipes.com/recipes/chicken_parmesan/',
-                     'address_short':'simplyrecipes.com' },
-            'body': 'Here is some more sample query text!'
-        },
-        {
-            'site': {'title': 'Franks Wing Dip',
-                     'address': 'https://www.franksredhot.com/recipes/franks-redhot-buffalo-chicken-dip-RE1242-1',
-                     'address_short':'urbandictionary.com' },
-            'body': 'Uh oh! We are not currently capturing any data to put here'
-        }
-    ]
+    posts = dead_query()
     ResultsCount = len(posts)
     TotalPages = ResultsCount // POSTS_PER_PAGE
     if (ResultsCount % POSTS_PER_PAGE > 0):
